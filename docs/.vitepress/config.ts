170e2bfaa36b465b11791c4ef39f50606a30e477
yuyou33 <1913438997@qq.com>
2025-03-08 16:46:11 +0800

    update_config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
   base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
 
   lang: 'zh-CN',
-  title: '茂茂物语',
-  description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
+  title: '雨柚',
+  description: 'Learn → Think → Code (Write) → Repeat',
   head,
 
   lastUpdated: true,
@@ -38,7 +38,7 @@ export default defineConfig({
       label: '目录',
     },
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996/vitepress-nav-template' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/yuyou33/yuyou-notes' }],
 
     footer: {
       message: '如有转载或 CV 的请标注本站原文地址',
