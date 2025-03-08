52a868e1f7d626c17b4500fe1f859a374c5e87d1
yuyou33 <1913438997@qq.com>
2025-03-08 21:05:26 +0800

    add_sidebar

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,3 +1,30 @@
 import type { DefaultTheme } from 'vitepress'
 
-export const sidebar: DefaultTheme.Config['sidebar'] = {}
+export const sidebar: DefaultTheme.Config['sidebar'] = {
+    '/编程学习/': [
+        { text: '导览', link: '/编程学习' },
+        {
+            text: 'C++',
+            Items: [
+                { text: 'CS106L', link: '/编程学习/C++/CS106l' }
+            ]
+        },
+        {
+            text: 'Linux',
+            Items: [
+                { text: 'vim', link: '/编程学习/Linux学习/001-vim' },
+                { text: 'linux常用命令', link: '/编程学习/Linux学习/002-linux常用命令' },
+                { text: 'The Missing Semester', link: '/编程学习/Linux学习/003-The Missing Semester' }
+            ]
+        }
+    ],
+    '/workflow/': [
+        { text: '导览', link: '/workflow' },
+        { text: 'Markdown', link: '/workflow/001-Markdown' },
+        { text: '正则表达式', link: '/workflow/002-正则表达式' }
+    ],
+    '/实用工具/': [
+        { text: '导览', link: '/实用工具'}
+    ]
+
+}
