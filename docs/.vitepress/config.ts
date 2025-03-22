82cbcccd9d44b937c5294cf5e0a748a704a2b1a1
yuyou33 <1913438997@qq.com>
2025-03-22 16:54:25 +0800

    docs: footer

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
 
     footer: {
       message: '如有转载或 CV 的请标注本站原文地址',
-      copyright: 'Copyright © 2019-present maomao',
+      copyright: 'Copyright © 2025 yuyou',
     },
 
     lastUpdated: {
